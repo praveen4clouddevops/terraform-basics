ab919468b945487130620e834d8ced5f1f459c82
telaprolup@gmail.com <AMMAnannatnp9#>
2023-07-13 07:52:48 +0530

    auto push

--- vars/02-vars.tf
+++ vars/02-vars.tf
@@ -26,3 +26,17 @@ variable "example_list"{
 output "example-list-op"{
     value= "Welcome to ${var.example_list[1]} with ${var.example_list[0]} Training and this is our batch ${var.example_list[2]} and the current topic is ${var.example_list[4]}"
 }
+
+#Map Example
+#A variable with multiple key value pairs is referred as map variavle
+
+variable "map_variable"{
+    default={
+        Batch    = "B54"
+        Training ="Devops with AWS"
+        mode     ="online"
+    }
+}
+output "example-map-op"{
+    value = "our training is ${var.map_variable{"Training"} and this is our ${var.map_variable{"Batch"} and it is ${var.map_variable{"mode"}"
+}
