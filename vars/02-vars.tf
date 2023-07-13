2ddf01dbf9c3f5f6123d5ed566612431ecb4cd73
telaprolup@gmail.com <AMMAnannatnp9#>
2023-07-13 07:40:04 +0530

    auto push

--- vars/02-vars.tf
+++ vars/02-vars.tf
@@ -9,3 +9,20 @@ output "sample-op"{
 output "sample-var-op"{
     value = "value of variable is ${var.sample}"
 }
+
+#list variable
+
+variable "example_list"{
+
+    default = [
+        "AWS",
+        "DEVOPS",
+        54,
+        "Trainer",
+        "Terraform"
+    ]
+}
+
+output "example-list-op"{
+    value= "Welcome to ${var.example_list[1]} with ${var.example_list[0]} Training and this is our batch ${var.example_list[2]} and the current topic is ${var.example_list[4]}"
+}
