dcf53a0814e9c2a35ffdf18ffbaa7a0b6c01a264
telaprolup@gmail.com <AMMAnannatnp9#>
2023-07-13 08:30:35 +0530

    auto push

---
+++ 03-vars/sample.tf
@@ -0,0 +1,20 @@
+# Declaring an empty variable city 
+
+variable "city" {}
+
+output "city_name" {
+    value = "Our city name is ${var.city}"
+}
+
+
+variable "state" {}
+
+output "state_name" {
+    value  = "Out State name is ${var.state}"
+}
+
+variable "coldcity" {}
+
+output "cold_city_output" {
+    value = " cold city value is ${var.coldcity} "
+}
