b3e2f84ff512724a9fe753922f283ad3cbda0d59
telaprolup@gmail.com <AMMAnannatnp9#>
2023-07-18 14:55:44 +0530

    auto push

---
+++ 05-remote-backend/backend.tf
@@ -0,0 +1,9 @@
+# Now the statefile is remote and this is what we use in a team based appraoch
+
+terraform {
+  backend "s3" {
+    bucket         = "b54-tf-remote-state-pt"
+    key            = "05-remote/terraform.tfstate"     # This is like the folder path that you prefer giving
+    region         = "us-east-1"
+  }
+}
