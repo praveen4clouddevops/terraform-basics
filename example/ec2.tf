6b1b37bc25e764b787319024e817bb64d03a0bdd
telaprolup@gmail.com <AMMAnannatnp9#>
2023-07-18 15:52:51 +0530

    auto push

--- example/ec2.tf
+++ example/ec2.tf
@@ -8,7 +8,7 @@
 # } Sample script for creating instance
 
 # Creates EC2
-resource "aws_instance" "server" {
+resource "aws_instance" "app_server" {
   ami                     = "ami-0c1d144c8fdd8d690"
   instance_type           = "t3.nano"
   vpc_security_group_ids  = [aws_security_group.allow_all.id]
