50cb3c7f2c4f9ba5075b5e4ab4fd5f26821828b3
telaprolup@gmail.com <AMMAnannatnp9#>
2023-07-18 16:47:03 +0530

    auto push

--- example2/ec2/main.tf
+++ example2/ec2/main.tf
@@ -1,10 +1,9 @@
-# Creates EC2
 resource "aws_instance" "sample" {
-  ami                     = data.aws_ami.image.id
+  ami                     = "ami-0c1d144c8fdd8d690"
   instance_type           = "t3.nano"
   vpc_security_group_ids  = [var.sg]
-}
 
+}
 variable "sg" {}   # step 3 : Now to use this info, declare an empty varianble and user it ( in root module, we have declared " sg = var.sgid )
 
 output "public_ip" {
