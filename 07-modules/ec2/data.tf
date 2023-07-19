fbf7d5ce92fa374685382ba474c81d37f06ac2f4
telaprolup@gmail.com <AMMAnannatnp9#>
2023-07-19 14:58:00 +0530

    auto push

--- 07-modules/ec2/data.tf
+++ 07-modules/ec2/data.tf
@@ -1,5 +1,5 @@
 data "aws_ami" "image" {
   most_recent      = true
-  name_regex       = "DevOps-LabImage-CentOS7"
-  owners          = ["355449129696"]
+  name_regex       = "Ansible-Image"     # Ensure you make your own AMI from my labImage and install ANSIBLE on the top of it and then use your AMI Name
+  owners           = ["self"]
 }
