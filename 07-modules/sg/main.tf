c6f5e690a01967111491e99b32297694c48f32d6
telaprolup@gmail.com <AMMAnannatnp9#>
2023-07-19 14:51:40 +0530

    auto push

--- 07-modules/sg/main.tf
+++ 07-modules/sg/main.tf
@@ -3,9 +3,9 @@ resource "aws_security_group" "allows_all" {
   description = "Allow TLS inbound traffic"
 
   ingress {
-    description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
