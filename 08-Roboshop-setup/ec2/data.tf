45e2f9198b9ad07147e7bd94b54a53a4dbe90717
telaprolup@gmail.com <AMMAnannatnp9#>
2023-07-24 15:40:43 +0530

    auto push

--- 08-Roboshop-setup/ec2/data.tf
+++ 08-Roboshop-setup/ec2/data.tf
@@ -1,5 +1,5 @@
 data "aws_ami" "image" {
   most_recent      = true
-  name_regex       = "ansible-lab-image"     # Ensure you make your own AMI from my labImage and install ANSIBLE on the top of it and then use your AMI Name
+  name_regex       = "Ansible-Image"     # Ensure you make your own AMI from my labImage and install ANSIBLE on the top of it and then use your AMI Name
   owners           = ["self"]
 }
